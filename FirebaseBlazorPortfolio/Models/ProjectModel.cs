2d086e754b70c1f2ddff966f3463c6c33c83b240
mubuyuk <muratbuyuksal@gmail.com>
2025-04-26 19:19:55 +0200

    Add project files.

---
+++ FirebaseBlazorPortfolio/Models/ProjectModel.cs
@@ -0,0 +1,17 @@
+﻿using Google.Cloud.Firestore;
+
+namespace MyPortfolio.Models
+{
+    [FirestoreData]
+    public class Skill
+    {
+        [FirestoreProperty]
+        public string TechName { get; set; }
+
+        [FirestoreProperty]
+        public string Category { get; set; }
+
+        [FirestoreProperty]
+        public string ImageUrl { get; set; }
+    }
+}
---
+++ FirebaseBlazorPortfolio/Services/FirestoreService.cs
@@ -0,0 +1,38 @@
+﻿using Google.Cloud.Firestore;
+using MyPortfolio.Models;
+
+namespace MyPortfolio.Services
+{
+    public class FirestoreService
+    {
+        private readonly FirestoreDb _firestoreDb;
+
+        public FirestoreService(IWebHostEnvironment env)
+        {
+            // Hämta sökvägen till din Firebase JSON-nyckel
+            string path = Path.Combine(env.ContentRootPath, "Keys", "portfolio-17515-firebase-adminsdk-fbsvc-d9017fc234.json");
+            Environment.SetEnvironmentVariable("GOOGLE_APPLICATION_CREDENTIALS", path);
+
+            // Anslut till Firestore (byt till ditt riktiga Firebase-projekt-ID)
+            _firestoreDb = FirestoreDb.Create("portfolio-17515");
+        }
+
+        public async Task<List<Skill>> GetSkillsAsync()
+        {
+            Query query = _firestoreDb.Collection("skills");
+            QuerySnapshot snapshot = await query.GetSnapshotAsync();
+
+            List<Skill> skills = new();
+            foreach (DocumentSnapshot doc in snapshot.Documents)
+            {
+                if (doc.Exists)
+                {
+                    Skill skill = doc.ConvertTo<Skill>();
+                    skills.Add(skill);
+                }
+            }
+
+            return skills;
+        }
+    }
+}
